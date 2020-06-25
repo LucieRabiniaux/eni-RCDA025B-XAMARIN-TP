837eb0fc46ec999c92584adc79fdaf6b221b8047
Lucie Rabiniaux <lucie.rabiniaux.externe@cnp.fr>
2020-06-25 08:27:44 +0200

    TP Module 6

--- TPModule5/TPModule5/MainPage.xaml.cs
+++ TPModule5/TPModule5/MainPage.xaml.cs
@@ -4,18 +4,33 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TPModule5.Models;
 using Xamarin.Forms;
 
 namespace TPModule5
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
+ 
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+            var LoginForm = new LoginForm(
+                this.idTwitter,
+                this.password,
+                this.isRemind,
+                this.error,
+                this.btnLogin,
+                this.tweets,
+                this.loginForm); ; 
+
+            //gestion de l'évènement au clic sur le bouton "Se connecter"
+            this.btnLogin.Clicked += LoginForm.BtnLogin_Clicked;
+
         }
+
+
     }
 }
---
+++ TPModule5/TPModule5/Models/LoginForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using System.Diagnostics;
+using Switch = Xamarin.Forms.Switch;
+
+namespace TPModule5.Models
+{
+    class LoginForm
+    {
+        //prop en lecture seule uniquement
+        public Entry Login { get;}
+
+        public Entry Password { get; }
+
+        public Xamarin.Forms.Switch IsRemind { get; }
+
+        public Label Error { get; }
+
+        public View Tweets { get; }
+
+        public View Form { get; }
+
+        public LoginForm(Entry login, Entry password, Switch isRemind, Label error, Button btnLogin, View tweets, View form)
+        {
+            Login = login;
+            Password = password;
+            IsRemind = isRemind;
+            Error = error;
+            Tweets = tweets;
+            Form = form;
+        }
+
+        public void BtnLogin_Clicked(object sender, EventArgs e)
+        {
+            //stocker les valeurs des champs dans des variables avant des les traiter
+            String login = this.Login.Text;
+            String password = this.Password.Text;
+            String errorMessage = "";
+
+            //utiliser Debug.WriteLine (et non Console.WriteLine)
+            Debug.WriteLine("Login button is clicked");
+
+            //vérification des conditions de validité
+            if(String.IsNullOrEmpty(login) || login.Length < 3)
+            {
+                errorMessage = "Veuillez saisir un identifiant d'au moins 3 caractères.";
+            }
+            if(String.IsNullOrEmpty(password) || password.Length < 6)
+            {
+                errorMessage += "Veuillez saisir un mot de passe d'au moins 6 caractères.";
+            }
+
+            //si le message d'erreur est non null : on affiche l'erreur.
+            //si pas d'erreur : masque le formulaire de connexion et on affiche les tweets.
+            if (!String.IsNullOrEmpty(errorMessage))
+            {
+                this.Error.Text = errorMessage;
+                this.Error.IsVisible = true;
+            } else
+            {
+                this.Form.IsVisible = false;
+                this.Tweets.IsVisible = true;
+            }
+
+
+        }
+
+
+    }
+
+    
+
+}
